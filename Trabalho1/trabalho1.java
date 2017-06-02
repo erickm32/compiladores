17f54af03b45b76166d49de73e8d2786015e2666
Erick Moreira <efmoreira@inf.ufpel.edu.br>
2017-06-02 10:44:36 -0300

    Correção final.

--- Trabalho1/trabalho1.java
+++ Trabalho1/trabalho1.java
@@ -9,6 +9,7 @@ class Trabalho {
 		 	arquivo = new BufferedReader(new FileReader(args[0]));
 
 		 	Stack<Integer> pilha = new Stack();
+		 	int op1, op2;
 
 		 	String linha = arquivo.readLine();
 		 	while(linha != null){
@@ -19,25 +20,30 @@ class Trabalho {
 		 			pilha.push(new Integer(splitted[1]));
 		 		}
 		 		else if (first.equalsIgnoreCase("ADD")) {
-		 			pilha.push(new Integer(pilha.pop() + pilha.pop()));
+		 			op2 = pilha.pop().intValue();
+		 			op1 = pilha.pop().intValue();
+		 			pilha.push(new Integer(op1 + op2));
 		 		}
 		 		else if (first.equalsIgnoreCase("SUBT")) {
-		 			pilha.push(new Integer(pilha.pop() - pilha.pop()));
+		 			op2 = pilha.pop().intValue();
+		 			op1 = pilha.pop().intValue();
+		 			pilha.push(new Integer(op1 - op2));
 		 		}
 		 		else if (first.equalsIgnoreCase("MULT")) {
-		 			pilha.push(new Integer(pilha.pop() * pilha.pop()));
+		 			op2 = pilha.pop().intValue();
+		 			op1 = pilha.pop().intValue();
+		 			pilha.push(new Integer(op1 * op2));
 		 		}
 		 		else if (first.equalsIgnoreCase("DIV")) {
-		 			pilha.push(new Integer(pilha.pop() * pilha.pop()));
+		 			op2 = pilha.pop().intValue();
+		 			op1 = pilha.pop().intValue();
+		 			pilha.push(new Integer(op1 / op2));
 		 		}
 		 		else if (first.equalsIgnoreCase("PRINT")) {
-		 			System.out.println(pilha.peek());
-		 		}
-		 		else if (first == "\n"){
-		 			System.out.println("\\n.");
+		 			System.out.println(pilha.peek().intValue());
 		 		}
 		 		else {
-		 			System.out.println("erro: '" + first + "'");
+		 			System.out.println("erro: '" + linha + "'");
 		 		}
 
 		 		linha = arquivo.readLine();
